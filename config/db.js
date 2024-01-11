91d9c87d57ee6358a05387a8eef1efb068e15161
Roman Abreu <abreulr19@gmail.com>
2024-01-11 12:06:41 -0600

    fix

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ dotenv.config({ path: '.env' });
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD ?? '', {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    // port: process.env.DB_PORT,
+    port: 3307,
     dialect: 'mysql',
     define: {
         timestamps: true
